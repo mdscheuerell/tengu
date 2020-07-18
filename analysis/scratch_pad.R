9c00547b17c218f09852e340fb246e3126522d87
mdscheuerell <scheuerl@uw.edu>
2020-07-17 17:28:26 -0700

    more scratching

--- analysis/scratch_pad.R
+++ analysis/scratch_pad.R
@@ -5,23 +5,30 @@ tt <- 70
 
 ee <- rep(NA, tt)
 
-dd <- sample(seq(5,10), tt, replace = TRUE)
-aa <- sample(seq(100,150), tt, replace = TRUE)
+cc <- matrix(NA, 100, 2)
 
-for(t in 1:tt) {
-  ee[t] <- sum(sample(seq(dd[t]), aa[t], replace = TRUE))
+for(i in 1:100) {
+  
+  dd <- sample(seq(2,13), tt, replace = TRUE)
+  aa <- sample(seq(27,190), tt, replace = TRUE)
+  
+  for(t in 1:tt) {
+    ee[t] <- sum(sample(seq(dd[t]), aa[t], replace = TRUE))
+  }
+  
+  cc[i,1] <- cor(dd, ee)
+  cc[i,2] <- cor(aa, ee)
+
 }
 
+## median & 90% quantiles
+quantile(cc[,1], c(5, 50, 95)/100)
+quantile(cc[,2], c(5, 50, 95)/100)
 
-
-
-
-mm <- aa * dd/2
-
-plot.ts(cbind(dd, aa, ee, mm))
+plot.ts(cbind(dd, aa, ee))
 
 corrplot::corrplot(cor(cbind(dd, aa, ee)))
 
 
-
+plot.ts(cbind(raw_data$days, raw_data$members))
 
